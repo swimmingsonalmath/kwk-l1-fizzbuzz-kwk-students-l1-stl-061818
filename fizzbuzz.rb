c4fbbd98b72d453cdbd0fd3ace4e0907a8d7cbfa
Sonali Sharma <sonalisharma58521@gmail.com>
2018-07-02 18:02:16 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -13,11 +13,11 @@
 # if a number is divisible by 3, return "fizz"
 def fizzbuzz(x)
   if x%5 == 0 && x%3 == 0 
-  puts "fizzbuzz"
+  puts "FizzBuzz"
   elsif x%3 == 0 # can also x%3 == 0 && x%5 !=0
-  puts "fizz"
+  puts "Fizz"
   elsif x%5 == 0 
-  puts "buzz"
+  puts "Buzz"
   end
 end
 
